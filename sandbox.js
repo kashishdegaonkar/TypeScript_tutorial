741f2920cb95dcf27f337467a7fb7a4e5845ae26
kashishdegaonkar <kashishdegaonkar11@gmail.com>
2023-06-26 13:25:07 +0530

    ts array,mixed arrays and objects.

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,16 @@
-//string         
-var character = "kashish";
-//number
-var age = 18;
-//boolean
-var isSmart = true;
-//any
-var myname;
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(8));
+var invitedNames = ['kashish', 'gargi', 'riya'];
+invitedNames.push('tanvi');
+for (var i = 0; i <= invitedNames.length; i++) {
+    console.log(invitedNames[i]);
+}
+//invitedNames.push(7); 
+//Argument of type 'number' is not assignable to parameter of type 'string'.
+var mixedArray = ['parth', 18, true, 'jadhav'];
+for (var i = 0; i <= mixedArray.length; i++) {
+    console.log(mixedArray[i]);
+}
+mixedArray.push('kashish');
+mixedArray[0] = 'ghogre';
+for (var i = 0; i <= mixedArray.length; i++) {
+    console.log(mixedArray[i]);
+}
--- sandbox.ts
+++ sandbox.ts
@@ -1,17 +1,36 @@
-//string         
-let character = "kashish"; 
+//ARRAYS
 
-//number
-let age = 18;    
+let invitedNames = ['kashish' , 'gargi' , 'riya'];
+invitedNames.push('tanvi'); 
 
-//boolean
-let isSmart = true;
-
-//any
-let myname;
-
-const circ = (diameter:number) => {
-    return diameter * Math.PI;
+for(var i = 0 ; i<=invitedNames.length ; i++){
+    console.log(invitedNames[i]);
 }
 
-console.log(circ(8));
+//invitedNames.push(7); 
+//Argument of type 'number' is not assignable to parameter of type 'string'.
+
+
+//MIXED-ARRAYS
+
+let mixedArray = ['parth' , 18 , true , 'jadhav'];
+for(var i = 0 ; i<=mixedArray.length ; i++){
+    console.log(mixedArray[i]);
+}
+
+mixedArray.push('kashish');
+mixedArray[0]='ghogre';
+for(var i = 0 ; i<=mixedArray.length ; i++){
+    console.log(mixedArray[i]);
+}
+
+
+//OBJECTS
+
+let student = {
+    studentName:'kashish',
+    age:18,
+    isCoding: true
+};
+
+//student.newSkill = "painting"; cannot add new objects explicitly.
